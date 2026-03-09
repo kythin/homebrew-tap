d3ab8726e23d38cc79512bb211d932e42ad51b15
Chris Samios <samios@gmail.com>
2026-03-09 03:34:47 +0000

    Promote stackydo to v0.4.2

--- Formula/stackydo.rb
+++ Formula/stackydo.rb
@@ -1,30 +1,30 @@
 class Stackydo < Formula
   desc "Context-aware CLI task manager with TUI"
   homepage "https://github.com/kythin/stackydo-cli"
-  version "0.1.0"
+  version "0.4.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://api.github.com/repos/kythin/stackydo-cli/releases/assets/358441149",
-          headers: ["Authorization: token #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}", "Accept: application/octet-stream"]
-      sha256 "a708dc97f0dc994ad96af1061a0029a1ab1ffd4189619d1bc375d1a83cbcfaf4"
+      url "https://api.github.com/repos/kythin/homebrew-tap/releases/assets/369823474",
+headers: ["Authorization: token #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}", "Accept: application/octet-stream"]
+      sha256 "75e6256c23db226dce712d4b45a6b0cd35c1054cdfc1dba04ecfacf05708fbc6"
     else
-      url "https://api.github.com/repos/kythin/stackydo-cli/releases/assets/358441163",
-          headers: ["Authorization: token #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}", "Accept: application/octet-stream"]
-      sha256 "0af10c01bc3b6f872458ce4cbcf5994ce2c12d9bc5f535a5047000c1c45a53a8"
+      url "https://api.github.com/repos/kythin/homebrew-tap/releases/assets/369823473",
+headers: ["Authorization: token #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}", "Accept: application/octet-stream"]
+      sha256 "742c096240931cd4a18c671fa4836a46a69707ebb27aacfdf7fcccf1d5f6ab9f"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://api.github.com/repos/kythin/stackydo-cli/releases/assets/358441153",
-          headers: ["Authorization: token #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}", "Accept: application/octet-stream"]
-      sha256 "2513a99919e1ee03e3d743f3c92ad2cd43df67233075f648b9d2c6dfbbc8a9c1"
+      url "https://api.github.com/repos/kythin/homebrew-tap/releases/assets/369823472",
+headers: ["Authorization: token #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}", "Accept: application/octet-stream"]
+      sha256 "3420053c32478e1e7101ef4b7dce0b94690916aa15364873924601f34a270abd"
     else
-      url "https://api.github.com/repos/kythin/stackydo-cli/releases/assets/358441171",
-          headers: ["Authorization: token #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}", "Accept: application/octet-stream"]
-      sha256 "d2804dee2ebbcd4a514f26f8246ce932274c017a489c92838ac21941e58b1400"
+      url "https://api.github.com/repos/kythin/homebrew-tap/releases/assets/369823471",
+headers: ["Authorization: token #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}", "Accept: application/octet-stream"]
+      sha256 "c655d8e051f9e9e1ff7e411f72ea31a1804eee72ac7c0eebf2f07600f93ee505"
     end
   end
 
