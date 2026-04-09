25d4cb041b6da457df48801e52a8f87504452394
Chris Samios <samios@gmail.com>
2026-04-09 04:57:26 +0000

    Update stackydo to v0.9.6

--- Formula/stackydo.rb
+++ Formula/stackydo.rb
@@ -1,26 +1,26 @@
 class Stackydo < Formula
   desc "Context-aware CLI task manager with MCP server"
   homepage "https://github.com/kythin/stackydo"
-  version "0.9.5"
+  version "0.9.6"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kythin/stackydo/releases/download/v0.9.5/stackydo-aarch64-apple-darwin.tar.xz"
-      sha256 "d12e3c21e07557a3e3fff8650cf78bf36583bd499ca051b88728337ef21cdb7f"
+      url "https://github.com/kythin/stackydo/releases/download/v0.9.6/stackydo-aarch64-apple-darwin.tar.xz"
+      sha256 "9dbfedaa1d4517da48fb7f5a031f57159a02c32a5da3f53c3d5821e38682b0f7"
     elsif Hardware::CPU.intel?
-      url "https://github.com/kythin/stackydo/releases/download/v0.9.5/stackydo-x86_64-apple-darwin.tar.xz"
-      sha256 "157deb0655d594870017ccef524be9857c0de355e1c27c48017d4f5857294784"
+      url "https://github.com/kythin/stackydo/releases/download/v0.9.6/stackydo-x86_64-apple-darwin.tar.xz"
+      sha256 "52e0ef6fae5d5e0dafa5faf91f2e00d6ffe7146a5ea216d2d2e00d89302d733b"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/kythin/stackydo/releases/download/v0.9.5/stackydo-aarch64-unknown-linux-gnu.tar.xz"
-      sha256 "2342ecb03b9dbf95095d23de8488fb378e9edc4f4af37fbc5dc9a9f552fb4611"
+      url "https://github.com/kythin/stackydo/releases/download/v0.9.6/stackydo-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "a4a69a01436dfa93b02730b8ddf81f04a62e092fd0c90a2634027f195614e649"
     elsif Hardware::CPU.intel?
-      url "https://github.com/kythin/stackydo/releases/download/v0.9.5/stackydo-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "95bf85ec008611806b38492d698e5a7fac299cb1a81ffbff2425d595920b216d"
+      url "https://github.com/kythin/stackydo/releases/download/v0.9.6/stackydo-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "e4cb2a3538517cbac27ffc5fe5197a3f1eff34235148d137764e1cf9fc06006a"
     end
   end
 
